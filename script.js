5c148422588f12d1c7ebb083bbc2a5cabe7bd40f
Ramazan <inuazh.work@yandex.ru>
2023-05-06 08:45:42 +0300

    almost be done

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let info = new userinfo();
 
-function t1(){
-    console.log(info.pageOn());
-    console.log(info.referrer());
-    console.log(info.previousSites());
+// Функция для отображения данных на странице
+async function displayData() {
+  // Получаем данные и сохраняем их в переменные
+  let pageOn = await info.pageOn();
+  let referrer = await info.referrer();
+  let previousSites = await info.previousSites();
+  let dataCookies = await info.dataCookies();
+  let dataStorage = await info.dataStorage();
+  let sizeScreen = await info.sizeScreen();
+  let battery = await info.battery();
+  let ip = await info.ip();
+
+  // Отображаем данные на странице
+  document.getElementById("pageOn").innerText = "Page On: " + pageOn;
+  document.getElementById("referrer").innerText = "Referrer: " + referrer;
+  document.getElementById("previousSites").innerText = "Previous Sites: " + previousSites;
+  document.getElementById("dataCookies").innerText = "Data Cookies: " + dataCookies;
+  document.getElementById("dataStorage").innerText = "Data Storage: " + dataStorage;
+  document.getElementById("sizeScreen").innerText = "Screen Size: " + sizeScreen;
+  document.getElementById("battery").innerText = "Battery: " + battery;
+  document.getElementById("ip").innerText = "IP Address: " + ip;
 }
 
-t1();
+// Вызываем функцию для отображения данных
+displayData();
--- userinfo.js
+++ userinfo.js
@@ -1,11 +1,11 @@
-class userinfo{
-    constructor(){
+class userinfo {
+    constructor() {
         this.timeOpened = new Date();
-        this.timezone = (new Date()).getTimezoneOffset()/60;
+        this.timezone = (new Date()).getTimezoneOffset() / 60;
 
     };
 
-    pageOn(){
+    pageOn() {
         /**
          * file location
          * 
@@ -13,17 +13,81 @@ class userinfo{
         return window, location.pathname;
     };
 
-    referrer(){
+    referrer() {
         // returns url of the page, linked tjis page
 
         return document.referrer
     };
 
-    previousSites(){
+    previousSites() {
         //number of elements in this session
 
         return history.length;
     };
 
-    
+    browserInfo() { return navigator; }
+
+    dataCookies() {
+        return decodeURIComponent(document.cookie.split(";"))
+    };
+
+    dataStorage() {
+        return localStorage
+    };
+
+    sizeScreen() {
+        return {
+            width: screen.width,
+            height: screen.height,
+            clientWidth: document.body.clientWidth,
+            clientHeight: document.body.clientHeight,
+            innerWidth: window.innerWidth,
+            innerHeight: window.innerHeight,
+            screenAvailWidth: screen.availWidth,
+            screenAvailHeight: screen.availHeight,
+            colorDepth: screen.colorDepth,
+            pixelDepth: screen.pixelDepth
+
+        }
+    };
+
+    async position(){
+        const pos = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+      
+          return {
+            long: pos.coords.longitude,
+            lat: pos.coords.latitude,
+            accuracy: pos.coords.accuracy,
+            altitude: pos.coords.altitude,
+            heading : pos.coords.heading,
+            speed : pos.coords.speed,
+            timestamp : pos.timestamp,
+          };
+    };
+
+    async battery(){
+        /**
+         * charging
+         * chargingTime
+         * level
+         */
+        return await navigator.getBattery();
+    };
+
+    async ip(){
+        /**
+         *  city
+         *  continent
+         *  countryCode
+         *  countryName
+         *  ipAddress
+         *  statProv
+         */
+        let res =  await fetch('https://api.db-ip.com/v2/free/self');
+        let data = await res.json();
+        return data;
+      
+    }
 }
